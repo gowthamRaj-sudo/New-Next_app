c785512d58b78171ae5dbb75353176b4496d7e19
gowthamRaj-sudo <gowthamraj87544@gmail.com>
2023-11-20 01:21:22 +0530

    changed corrected api

--- app/api.ts
+++ app/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://13.51.76.217:3001";
+export const baseUrl = "http://13.51.176.217:3001";
 
 export const instance=axios.create({
     baseURL:baseUrl
