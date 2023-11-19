b8ee68af23a0a74f8324f413c7ad154cf3fa2397
gowthamRaj-sudo <gowthamraj87544@gmail.com>
2023-11-19 18:01:24 +0530

    change the inputcomponent provide optional props ? this

--- app/component/InputComponent.tsx
+++ app/component/InputComponent.tsx
@@ -1,46 +1,66 @@
 'use client';
 import React, { ChangeEvent } from 'react'
 
-interface InputComponentProps{
-  type:string,
-  value:any,
-  onChange:any,
-  name:any
-  label:string
-  placeholder:string
-  email:any,
-  required:string
+interface InputComponentProps {
+  type: string;
+  value: any;
+  onChange: any;
+  name: any;
+  label: string;
+  placeholder: string;
+  email?: boolean;
 }
 interface EventpropsTypes{
   event:any
 }
 const emailFormat= /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const InputComponent :React.FC<InputComponentProps> = ({type,value,onChange,name,label,placeholder,email,required}) => {
+const InputComponent: React.FC<InputComponentProps> = ({
+  type,
+  value,
+  onChange,
+  name,
+  label,
+  placeholder,
+  email,
+}) => {
   return (
-    <div style={{padding:"15px"}}>
-      <label style={{textAlign:"end",color:"#333",fontWeight:"600"}}> {label}</label><br/>
-<input type={type} value={value} onChange={(e)=>{
-  // const inputValue=e.target.value
- 
-  return onChange(e)
-  
-}} name={name} placeholder={placeholder}style={{ 
-  // color: "#333",
-  fontSize: '1.2rem',
-	margin: "0 auto",
-  padding: "1.1rem 1rem",
-  borderRadius: "0.2rem",
-  // backgroundColor: "#FAFAFA",
-  width:" 90%",
+    <div style={{ padding: "15px" }}>
+      <label style={{ textAlign: "end", color: "#333", fontWeight: "600" }}>
+        {" "}
+        {label}
+      </label>
+      <br />
+      <input
+        type={type}
+        value={value}
+        onChange={(e) => {
+          // const inputValue=e.target.value
 
-  // borderBottom: "0.3rem solid transparent",
-  transition:" all 0.3s"}}/>
+          return onChange(e);
+        }}
+        name={name}
+        placeholder={placeholder}
+        style={{
+          // color: "#333",
+          fontSize: "1.2rem",
+          margin: "0 auto",
+          padding: "1.1rem 1rem",
+          borderRadius: "0.2rem",
+          // backgroundColor: "#FAFAFA",
+          width: " 90%",
 
-
-{email&&!emailFormat.test(value) ?<p style={{color:"red",fontSize:"13px"}}>Invalid Email format</p>:""
-}    </div>
-  )
-}
+          // borderBottom: "0.3rem solid transparent",
+          transition: " all 0.3s",
+        }}
+      />
+      {email && !emailFormat.test(value) ? (
+        <p style={{ color: "red", fontSize: "13px" }}>Invalid Email format</p>
+      ) : (
+        ""
+      )}{" "}
+    </div>
+  );
+};
 
 export default InputComponent
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -50,24 +50,61 @@ toast("Created Succesfully !",{
 
 
   return (
-    <> 
-    <Layout>
-    {/* <ul>{state?.map((e,i)=><li >{e.name}</li>)}</ul> */}
-    <div style={{background:"white",boxShadow:" rgba(0, 0, 0, 0.16) 0px 1px 4px"}}>
-    <h3 style={{padding:"15px",textAlign:"center"}}>CREAT USERS</h3>
+    <>
+      <Layout>
+        {/* <ul>{state?.map((e,i)=><li >{e.name}</li>)}</ul> */}
+        <div
+          style={{
+            background: "white",
+            boxShadow: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+          }}
+        >
+          <h3 style={{ padding: "15px", textAlign: "center" }}>CREAT USERS</h3>
 
-    <div className={createStyle.inputSection}><InputComponent label='UserName' type='text' value={CreateData.name} onChange={handleChange} name={"name"} placeholder='Enter the UserName'/>
-    <InputComponent label='Email' type='text' value={CreateData.password} onChange={handleChange} name={"password"} placeholder='Enter the Email' email required/>
-    <InputComponent label='FatherName' type='text' value={CreateData.fatherName} onChange={handleChange} name={"fatherName"} placeholder='Enter the FatherName'/>
-    <InputComponent  label='MotherName' type='text' value={CreateData.motherName} onChange={handleChange} name={"motherName"} placeholder='Enter the MotherName'/></div>
-    <div style={{padding:"15px"}}><button onClick={PostUsersDetails} className={createStyle.button}>Create</button></div>
-
-
-    </div>
-    </Layout>
+          <div className={createStyle.inputSection}>
+            <InputComponent
+              label="UserName"
+              type="text"
+              value={CreateData.name}
+              onChange={handleChange}
+              name={"name"}
+              placeholder="Enter the UserName"
+            />
+            <InputComponent
+              label="Email"
+              type="text"
+              value={CreateData.password}
+              onChange={handleChange}
+              name={"password"}
+              placeholder="Enter the Email"
+              email
+            />
+            <InputComponent
+              label="FatherName"
+              type="text"
+              value={CreateData.fatherName}
+              onChange={handleChange}
+              name={"fatherName"}
+              placeholder="Enter the FatherName"
+            />
+            <InputComponent
+              label="MotherName"
+              type="text"
+              value={CreateData.motherName}
+              onChange={handleChange}
+              name={"motherName"}
+              placeholder="Enter the MotherName"
+            />
+          </div>
+          <div style={{ padding: "15px" }}>
+            <button onClick={PostUsersDetails} className={createStyle.button}>
+              Create
+            </button>
+          </div>
+        </div>
+      </Layout>
     </>
-   
-  )
+  );
 }
 
 export default page
